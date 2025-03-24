7d3a9551ea5e47e33da69a2ae3fd115647bd33da
jaimeescartinsv <a27421@svalero.com>
2025-03-24 23:47:47 +0100

    dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/DashboardView.vue'
 import LandingPage from '../views/LandingPage.vue'
 
 const router = createRouter({
