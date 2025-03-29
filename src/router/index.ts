b4ed27e4abbfcbd39593c4ce213188141ee6abeb
jaimeescartinsv <a27421@svalero.com>
2025-03-29 20:05:48 +0100

    ProfileView

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,7 @@ import StringerDetails from '@/components/StringerDetails.vue'
 import TournamentsView from '@/views/TournamentsView.vue'
 import TournamentDetails from '@/components/TournamentDetails.vue'
 import ReportsView from '@/views/ReportsView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,12 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: HomeView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
       meta: { requiresAuth: true }
     },
     {
